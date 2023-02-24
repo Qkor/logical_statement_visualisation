06d9459ae0055d9b7a1bbae2f15830940983604c
jk <jan.kukorowski@wp.pl>
2023-02-24 12:55:18 +0100

    negation

--- src/modify.js
+++ src/modify.js
@@ -4,16 +4,42 @@ import './modify.css'
 const Modify = ({index}) => {
   
   const {formula, setFormula} = useContext(formulaContext)
+  const parentIndex = formula.findIndex(i => i.id === formula[index].parent)
 
   if(formula[index].id === 0)
     return <></>
 
   const rmvFormula = ()=>{} //to be implemented
-  const notFormula = ()=>{} //to be implemented
+  const notFormula = ()=>{
+    let newFormula = formula.slice()
+    if(newFormula[index].type === 'NOT'){
+      let childIndex = newFormula.findIndex(i => i.id === newFormula[index].children[0])
+      const thisId = newFormula[index].id
+      newFormula[index] = {...newFormula[childIndex]}
+      newFormula[index].id = thisId
+      newFormula[childIndex].id = -1
+      newFormula = newFormula.filter(i=>i.id!==-1)
+    }
+    else if(newFormula[parentIndex].type === 'NOT'){
+      newFormula[index].parent = newFormula[parentIndex].parent
+      newFormula[index].id = newFormula[parentIndex].id
+      newFormula[parentIndex].id = -1
+      newFormula = newFormula.filter(i=>i.id!==-1)
+    }
+    else{
+      let cp = {...newFormula[index]}
+      cp.parent = newFormula[index].id
+      const newId = (new Date()).getTime()
+      cp.id = newId
+      newFormula[index].type = 'NOT'
+      newFormula[index].children = [newId]
+      newFormula.push(cp) 
+    }
+    setFormula(newFormula)
+  } 
 
   const addFormula = (type)=>{
     const newFormula = formula.slice()
-    const parentIndex = newFormula.findIndex(i => i.id === newFormula[index].parent)
 
     const newId = (new Date()).getTime()
     let newThesis ={
